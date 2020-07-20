d0d08538ce7b1051ab002f6c54f073277af1911f
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-07-20 17:17:29 +0200

    update new version

--- functionList.py
+++ functionList.py
@@ -4,7 +4,7 @@ import logging as lg
 
 class listTransformation:
     """
-    A class used to represent a collection of query strings and a collection of input strings
+    A class used to represent a list as input
 
     ...
 
@@ -31,13 +31,7 @@ class listTransformation:
         """
         self.inputList = inputList
 
-def left_rotation(d,l):
-    n = len(self.inputList)
-    k = n - d
-    b = []
-    for i in range(0,n):
-        b.append(l[i-k])      
-    return b
+
 
     
     def left_rotation(d,self):
@@ -63,5 +57,37 @@ def left_rotation(d,l):
         for i in range(0,n):
             b.append(l[i-k])      
         return b
+
+
+    
+    def arrayManipulation(d,self):
+        """
+        Given an array of n integers and a number, d, perform  left rotations on the array
+        A left rotation operation on an array of size n shifts each of the array's elements  1 unit to the left. 
+        For example, if  2-left rotations are performed on array [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2].
+
+        :return: shifted array
+        :rtype: array[int]
+        
+        :Example:
+        >>> n = 5
+        >>> d = 4
+        >>> a = [i for i in range(1,n+1)]   
+        >>> left_rotation(4,a)
+        >>> [5, 1, 2, 3, 4]
+        """
+        queries = self.queries
+        a = np.array([0]*d)
+        k = len(self.queries)
+        for i in range(0,k): 
+            a[int(self.queries[i][0])-1:int(self.queries[i][1])] += int(self.queries[i][2]) 
+        return max(a)
+
+
+
+
+
+
+
 
 
