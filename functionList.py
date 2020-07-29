59657fed84a6cbe011204a9d2e79ad268e0c8d48
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-07-29 21:12:53 +0200

    Update functionList.py

--- functionList.py
+++ functionList.py
@@ -88,14 +88,28 @@ class listTransformation:
 
 
     def decryptPassword(self):
+        """
+        Given an array of n integers and a number, d, perform  left rotations on the array
+        A left rotation operation on an array of size n shifts each of the array's elements  1 unit to the left. 
+        For example, if  2-left rotations are performed on array [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2].
+
+        :return: shifted array
+        :rtype: array[int]
+        
+        :Example:
+        >>> s = 'hAck3rr4nk'
+        >>> decryptPassword(s)
+        >>>
+        >>> s = 'poTaTo' 
+        >>> decryptPassword(s)
+        >>> ['p', 'T', 'o', '*', 'T', 'a', '*', 'o']
+        """
         l = [char for char in self]
         m = len([letter for letter in l if letter.isupper()])
         k = len(l)
         w = []
         b = []
-        for i in list(range(k)): 
-            print(i)
-            print(l[i])        
+        for i in list(range(k)):        
             if i< k -2 and l[i].islower() and l[i+1].isupper():
                w.append(l[i+1]) 
                w.append(l[i])           
@@ -111,4 +125,52 @@ class listTransformation:
         return d
 
 
+    def hourglassSum(self):
+        """
+        Given a 6 x 6  2D Array, "arr":
+        We define an hourglass in A to be a subset of values with indices falling in this pattern in "arr"'s graphical representation:
+        For example, 
 
+        a b c
+          d
+        e f g
+
+        There are 16 hourglasses in "arr", and an hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in "arr", 
+        then print the maximum hourglass sum.
+
+        For example, given the 2D array:
+        -9 -9 -9  1 1 1 
+         0 -9  0  4 3 2
+        -9 -9 -9  1 2 3
+         0  0  8  6 6 0
+         0  0  0 -2 0 0
+         0  0  1  2 4 0
+
+        We calculate the following  hourglass values:
+
+        -63, -34, -9, 12, -10, 0, 28, 23, -27, -11, -2, 10, 9, 17, 25, 18
+
+        Our highest hourglass value is 28 from the hourglass:
+
+        0 4 3
+          1
+        8 6 6
+
+        
+        :return: the maximum hourglass sum.
+        :rtype: int
+        
+        :Example:
+        >>> T = [[-9,-9,-9,1,1,1],[0,-9,0,4,3,2],[-9,-9,-9,1,2,3],[0,0,8,6,6,0],[0,0,0,-2,0,0],[0,0,1,2,4,0]]
+        >>> hourglassSum(T)
+        >>> 28
+        """
+
+
+        a = []
+        for  c in list(range(0,4)):
+            for  l in list(range(0,4)):
+                Matrix = [[self[i][j] for  j in range(c,c+3)]   for i in range(l,l+3)] 
+                print(Matrix)
+                a.append(sum(sum(Matrix,[])) -  Matrix[1][0] -  Matrix[1][2])
+        return max(a)
